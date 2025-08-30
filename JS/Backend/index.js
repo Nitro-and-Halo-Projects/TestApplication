b843fdd74582366c8d693070c563b62f59bf4404
Nitro-Live <100413904+BytePilotX@users.noreply.github.com>
2025-08-30 01:16:50 +0100

    Adding GET and POST functions

--- JS/Backend/index.js
+++ JS/Backend/index.js
@@ -1,25 +1,33 @@
-const express = require('express')();
+
+const express = require('express');
+const app = express();
+app.use( express.json() );
 const cors = require('cors');
 const PORT = 8080;
-express.use(cors({
-    origin: "http://localhost:5500"
+app.use(cors({ // Middleware allowing for API requests from authorized sources
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 console.log("running");
-express.listen(
+app.listen(
     PORT,
     () => console.log(`Its alive on: http://localhost:${PORT}`)
 )
 
-express.get('/test', (req, res) => {
+app.get('/test', (req, res) => { // Simple GET request
     res.status(200).send({
         message: "Working!"
     })
 })
 
-express.post('/testpost', (req,res) => {
+app.post('/testpost', (req,res) => { // Basic body repeater
     const params = req.params;
     const body = req.body;
     res.status(200).send({
-        body: body
+        working: "true",
+        body: body,
+        params: req.params
     })
+    console.log(req.body);
 })
--- JS/FrontJS/button.js
+++ JS/FrontJS/button.js
@@ -1,28 +1,52 @@
-const { json } = require("body-parser");
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() { // Tells me when DOM is loaded
     console.log("DOM is loaded");
 });
 
-function login() {
-    const userTextField = document.getElementById("User");
-    const passTextField = document.getElementById("Pass");
-    //const loginButton = document.getElementById("Login");
+// Initialising DOM variables
+const userTextField = document.getElementById("User");
+const passTextField = document.getElementById("Pass");
+const loginButton = document.getElementById("Login");
+loginButton.addEventListener("click", login);
+
+function login() { // When button is clicked these functions are triggered
     console.log("Logging in...");
-    const userValue = userTextField.value;
-    const passValue = passTextField.value;
+    testGET();
+    testPOST();
     sendToBackend();
 }
 
-async function testApi() {
-    const api = await fetch("http://localhost:8080/test");
-    const formattedApiResponse = await api.json().then(
-        formattedApiResponse => {
-            console.log(formattedApiResponse.message);
-        }
-    )
+async function testGET() { // Sends a simple GET request to the Node.js backend
+    try {
+        const api = await fetch("http://localhost:8080/test", { method: 'GET' });
+        const data = await api.json()
+        console.log(await data.message);
+    } catch (e) {
+        console.log(e + "\n");
+        console.log("Have you started hosting the backend?\n");
+    }
+};
+
+async function testPOST() { // Sends a POST request to the backend for it to be repeated back
+    const userString = `User: ${userTextField.value}\n Pass: ${passTextField.value}`
+    try{
+        const response = await fetch("http://localhost:8080/testpost", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: userString
+            })
+        });
+        const data = await response.json();
+        console.log(data.body.message);
+    } catch (e) {
+        console.log(e);
+        console.log("Error while trying to do a TEST POST REQUEST");
+        console.log("Have you started hosting the backend?\n");
+    }
 }
 
-async function sendToBackend() {
+async function sendToBackend() { // This function will be used to send the data to the backend for verification
    
-}
+};
