a3b6a2ace2d0a34bf4538c72a25c1fc9901f6be0
Nitro-Live <100413904+BytePilotX@users.noreply.github.com>
2025-08-31 01:32:39 +0100

    Animations, Navbar, and Backend Functionality!
    Made some nice CSS changes and added a nice status text for the backend communication!

--- JS/FrontJS/button.js
+++ JS/FrontJS/button.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() { // Tells me when DOM 
 const userTextField = document.getElementById("User");
 const passTextField = document.getElementById("Pass");
 const loginButton = document.getElementById("Login");
+const loginStatusText = document.getElementById("status");
 loginButton.addEventListener("click", login);
 
 function login() { // When button is clicked these functions are triggered
@@ -29,6 +30,7 @@ async function testGET() { // Sends a simple GET request to the Node.js backend
 async function testPOST() { // Sends a POST request to the backend for it to be repeated back
     const userString = `User: ${userTextField.value}\n Pass: ${passTextField.value}`
     try{
+        loginStatusText.innerHTML = "Logging you in... 🧾"
         const response = await fetch("http://localhost:8080/testpost", {
             method: "POST",
             headers: {
@@ -40,7 +42,13 @@ async function testPOST() { // Sends a POST request to the backend for it to be 
         });
         const data = await response.json();
         console.log(data.body.message);
+        if (data.body) {
+            loginStatusText.innerHTML = "Logged in! 📩"
+        } else {
+            loginStatusText.innerHTML = "Error while logging in 📲"
+        }
     } catch (e) {
+        loginStatusText.innerHTML = "Error while logging in 📲"
         console.log(e);
         console.log("Error while trying to do a TEST POST REQUEST");
         console.log("Have you started hosting the backend?\n");
