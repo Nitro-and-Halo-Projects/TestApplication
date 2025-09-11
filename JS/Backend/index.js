a0a3804dbe4a0be57a84372a1201433e96a7491c
Nitro-Live <100413904+BytePilotX@users.noreply.github.com>
2025-09-11 23:04:15 +0100

    Changing listening protocol

--- JS/Backend/index.js
+++ JS/Backend/index.js
@@ -25,7 +25,7 @@ app.use(cors({ // Middleware allowing for API requests from authorized sources
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 console.log("running");
-httpsServer.listen(
+app.listen(
     PORT,
     "0.0.0.0",
     messageConsole()
